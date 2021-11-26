cb47efd0e2d4463f46daec9bb41f939e86722350
Nic Henriksson <nhenriksson@invidi.se>
2021-11-26 10:29:47 +0100

    Updated dependencies. Changed target version to 31.

--- app/src/main/java/com/ooyala/pulseplayer/videoPlayer/VideoPlayerActivity.java
+++ app/src/main/java/com/ooyala/pulseplayer/videoPlayer/VideoPlayerActivity.java
@@ -6,8 +6,9 @@ import android.content.pm.ActivityInfo;
 import android.net.Uri;
 import android.os.Bundle;
 
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
