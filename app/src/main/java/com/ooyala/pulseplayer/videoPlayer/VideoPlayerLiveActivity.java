1ebed0452ccf792a4a6e1d9061972e999d0ce339
Muhammad Tayyab <mtayyab@invidi.com>
2025-04-10 13:46:53 +0200

    Layout for PlayerLive is Done but left with some conflicts for ExoPlayer and Media3

--- app/src/main/java/com/ooyala/pulseplayer/videoPlayer/VideoPlayerLiveActivity.java
+++ app/src/main/java/com/ooyala/pulseplayer/videoPlayer/VideoPlayerLiveActivity.java
@@ -6,59 +6,57 @@ import android.app.UiModeManager;
 import android.content.res.Configuration;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 
+import androidx.annotation.OptIn;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.media3.common.util.UnstableApi;
+import androidx.media3.exoplayer.ExoPlayer;
 import androidx.media3.ui.PlayerView;
 
-import com.ooyala.pulse.FriendlyObstruction;
 import com.ooyala.pulseplayer.PulseManager.PulseManagerLive;
 import com.ooyala.pulseplayer.R;
 import com.ooyala.pulseplayer.model.VideoItem;
 
-import java.util.ArrayList;
-import java.util.List;
-
-@UnstableApi
-public class VideoPlayerLiveActivity extends AppCompatActivity {
+@UnstableApi public class VideoPlayerLiveActivity extends AppCompatActivity {
     static final int OPEN_BROWSER_REQUEST = 1365;
     public static PulseManagerLive pulseManagerLive;
     private PlayerView playerView;
-    private Dialog mFullScreenDialog;
-    private ImageView mFullScreenIcon;
-    private FrameLayout mFullScreenButton;
     private View adView;
     private Button skipButton;
-    private ImageView nextAdThumbnail;
-    List<FriendlyObstruction> friendlyObs = new ArrayList<>();
-    private boolean mExoPlayerFullscreen = false;
-    private static final String TAG = VideoPlayerVODActivity.class.getName();
-    private   UiModeManager uiMode;
+    private UiModeManager uiMode;
 
-    @UnstableApi
+    @OptIn(markerClass = UnstableApi.class)
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_video_player);
 
+        try {
+            setContentView(R.layout.activity_video_player_live);
+        } catch (Exception e) {
+            Log.e("Alpha", "Layout inflation error", e);
+            for (Throwable t = e; t != null; t = t.getCause()) {
+                Log.e("Alpha", "Caused by: ", t);
+            }
+            throw e; // This ensures the full error appears in Logcat
+        }
         //Get the selected videoItem from the bundled information.
         final VideoItem videoItem = getSelectedVideoItem();
         skipButton = (Button) findViewById(R.id.skipBtn);
-        nextAdThumbnail = (ImageView) findViewById(R.id.nextAdThumbnail);
         skipButton.setVisibility(View.INVISIBLE);
-        playerView = findViewById(R.id.exoplayer);
+        playerView = findViewById(R.id.exoPlayerView);
+
         playerView.showController();
         playerView.setControllerShowTimeoutMs(-1);
         uiMode = (UiModeManager) getSystemService(UI_MODE_SERVICE);
         adView = findViewById(R.id.exo_content_frame);
 
-        if(uiMode.getCurrentModeType() == Configuration.UI_MODE_TYPE_TELEVISION) {
+        if (uiMode.getCurrentModeType() == Configuration.UI_MODE_TYPE_TELEVISION) {
             skipButton.setFocusableInTouchMode(true);
             skipButton.setFocusable(true);
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
