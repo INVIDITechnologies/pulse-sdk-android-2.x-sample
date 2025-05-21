1f815bcf133a9566c46cbb792ce64355e152ce06
Muhammad Tayyab <mtayyab@invidi.com>
2025-05-21 14:17:29 +0200

    Added comments to Application class to explain why and how to initialize the Pulse with host.

--- app/src/main/java/com/ooyala/pulseplayer/List/MainActivity.java
+++ app/src/main/java/com/ooyala/pulseplayer/List/MainActivity.java
@@ -54,25 +54,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             }
         });
 
-        // Initialize the Pulse SDK with "setPulseHost(Host, Device Container, Persistent Id)"
-        // Host:
-        //     Your Pulse account host
-        // Device Container:
-        //     Device container in INVIDI Pulse is used for targeting and
-        //     reporting purposes. This device container attribute is only used
-        //     if you want to override the Pulse device detection algorithm on the
-        //     Pulse ad server. This should only be set if normal device detection
-        //     does not work and only after consulting INVIDI personnel. An incorrect
-        //     device container value can result in no ads being served or incorrect
-        //     ad delivery and reports.
-        // Persistent Id:
-        //     The persistent identifier is used to identify the end user and is the
-        //     basis for frequency capping, uniqueness, DMP targeting information and
-        //     more. Use Apple's advertising identifier (IDFA), or your own unique
-        //     user identifier here.
-        // Refer to:
-        //     http://support.ooyala.com/developers/ad-documentation/oadtech/ad_serving/dg/integration_sdk_parameter.html
-      //  Pulse.setPulseHost("https://pulse-demo.videoplaza.tv", null, null);
 
         if (videoJSonString != null) {
 
--- app/src/main/java/com/ooyala/pulseplayer/PulseApplication.java
+++ app/src/main/java/com/ooyala/pulseplayer/PulseApplication.java
@@ -9,6 +9,27 @@ public class PulseApplication  extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+
+        // Initialize the Pulse SDK with "setPulseHost(Host, Device Container, Persistent Id) in Application class of your project. (Recommended way, so it can work with lifecycles of all TV and Mobile versions)"
+        // Host:
+        //     Your Pulse account host
+        // Device Container:
+        //     Device container in INVIDI Pulse is used for targeting and
+        //     reporting purposes. This device container attribute is only used
+        //     if you want to override the Pulse device detection algorithm on the
+        //     Pulse ad server. This should only be set if normal device detection
+        //     does not work and only after consulting INVIDI personnel. An incorrect
+        //     device container value can result in no ads being served or incorrect
+        //     ad delivery and reports.
+        // Persistent Id:
+        //     The persistent identifier is used to identify the end user and is the
+        //     basis for frequency capping, uniqueness, DMP targeting information and
+        //     more. Use Apple's advertising identifier (IDFA), or your own unique
+        //     user identifier here.
+        // Refer to:
+        //     http://support.ooyala.com/developers/ad-documentation/oadtech/ad_serving/dg/integration_sdk_parameter.html
+
         Pulse.setPulseHost("https://pulse-demo.videoplaza.tv", null, null);
     }
 }
